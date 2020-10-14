d0a661c790ff4feda96b39d9b5742e28c0dc6afe
hicham kerkri <hicham.kerkri.hk@gmail.com>
2020-10-14 22:11:46 +0200

    inheritance

---
+++ oop.py
@@ -0,0 +1,70 @@
+class Employee:
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@email.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
+
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('--->', emp.fullname())
+
+
+dev_1 = Developer('hicham', 'kerkri', 40000, 'python')
+dev_2 = Developer('Ayoub', 'kerkri', 50000, 'java')
+
+mng_1 = Manager('Laurent', 'gendrier', 60000, [dev_1, dev_2])
+mng_2 = Manager('Frc', 'GMr', 70000, [dev_1])
+
+# mng_1.print_emps()
+# print(dev_1)
+# print(repr(dev_1))
+
+#print(dev_1 + dev_2)
+
+print(len(dev_2))
